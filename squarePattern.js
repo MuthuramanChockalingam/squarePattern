f1dbad8ed6f5d00edbaab4fcf7ee5a7c1ab66e15
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-06-01 20:19:27 +0530

    Refactored for readability

--- squarePattern.js
+++ squarePattern.js
@@ -1,9 +1,16 @@
+const firstLine = (arr) => arr.join('');
+const middleLines = (arr) => arr.slice(1, -1).map(middleLine());
+const middleLine = () =>
+  (val, i, arr) => val + ' '.repeat(arr.length) + arr[arr.length - i - 1];
+const lastLine = (arr) => arr.slice().reverse().join('');
+
 const square = (arr) => [
-  arr.join(''),
-  ...arr.slice(1,-1).map((val,i,arr) => val + ' '.repeat(arr.length) + arr[arr.length - i - 1]),
-  arr.slice().reverse().join(''),
+  firstLine(arr),
+  ...middleLines(arr),
+  lastLine(arr),
 ].join('\n');
 
 console.log(square(['a', 'b']),'\n');
 console.log(square(['a', 'b', 'c']),'\n');
 console.log(square(['a', 'b', 'c', 'd', 'e']),'\n');
+
