d9f1dcfb9b4a3b2ffbf5bfacb312b63a7fccb4a7
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-06-01 20:00:02 +0530

    shrinked to one line function.

--- squarePattern.js
+++ squarePattern.js
@@ -1,16 +1,8 @@
-const square = (arr) => {
-
-  const firstLine = arr.join('');
-  const lastLine = arr.slice().reverse().join('');
-  const middleLine = arr.slice(1,-1).map((val,i,arr) => val + ' '.repeat(arr.length) + arr[arr.length - i - 1]);
-  const Lines = [
-    firstLine,
-    ...middleLine,
-    lastLine,
-  ]
-
-  return Lines.join('\n');
-}
+const square = (arr) => [
+  arr.join(''),
+  ...arr.slice(1,-1).map((val,i,arr) => val + ' '.repeat(arr.length) + arr[arr.length - i - 1]),
+  arr.slice().reverse().join(''),
+].join('\n');
 
 console.log(square(['a', 'b']),'\n');
 console.log(square(['a', 'b', 'c']),'\n');
